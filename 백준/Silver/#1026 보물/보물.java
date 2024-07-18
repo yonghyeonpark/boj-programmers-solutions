666503f848102ba95c14dad4edaa8d26688bf8a0
Yong Hyeon <126778700+yonghyeonpark@users.noreply.github.com>
2024-07-19 01:14:34 +0900

    [Silver IV 1026] Title: 보물, Time: 124 ms, Memory: 14296 KB -NKLCBHub

---
+++ 백준/Silver/#1026 보물/보물.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] a = new Integer[n];
+        Integer[] b = new Integer[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(a);
+        Arrays.sort(b, Collections.reverseOrder());
+        
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result += a[i] * b[i];
+        }
+        System.out.println(result);
+    }
+}
