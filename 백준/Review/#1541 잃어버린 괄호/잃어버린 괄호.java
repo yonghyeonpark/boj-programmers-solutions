0b6945f6510284a6f82017ccb36464ec750d9deb
Yong Hyeon <126778700+yonghyeonpark@users.noreply.github.com>
2024-08-22 00:36:58 +0900

    [Silver II 1541] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14320 KB -NKLCBHub

---
+++ 백준/Review/#1541 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] subtractionParts = br.readLine().split("-");
+        List<Integer> additionResults = new ArrayList<>();
+        for (String subtractionPart : subtractionParts) {
+            int sum = 0;
+            String[] additionParts = subtractionPart.split("\\+");
+            for (String additionPart : additionParts) {
+                sum += Integer.parseInt(additionPart);
+            }
+            additionResults.add(sum);
+        }
+
+        int result = additionResults.get(0);
+        for (int i = 1; i < additionResults.size(); i++) {
+            result -= additionResults.get(i);
+        }
+
+        System.out.println(result);
+    }
+}
