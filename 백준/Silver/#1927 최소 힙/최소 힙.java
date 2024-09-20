953ef53f6650c942f24b5347d8c6a24021c8ef27
Yong Hyeon <126778700+yonghyeonpark@users.noreply.github.com>
2024-09-20 20:41:52 +0900

    [Silver II 1927] Title: 최소 힙, Time: 776 ms, Memory: 34128 KB -NKLCBHub

---
+++ 백준/Silver/#1927 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (pQ.isEmpty()) {
+                    System.out.println(0);
+                    continue;
+                }
+                System.out.println(pQ.poll());
+                continue;
+            }
+            pQ.offer(num);
+        }
+    }
+}
