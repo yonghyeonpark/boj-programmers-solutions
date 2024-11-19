3c571c3a150dae9919b4cdeca6b65af773c2a901
Yong Hyeon <126778700+yonghyeonpark@users.noreply.github.com>
2024-11-19 21:33:02 +0900

    [Silver V 2751] Title: 수 정렬하기 2, Time: 1512 ms, Memory: 167576 KB -NKLCBHub

---
+++ 백준/Silver/#2751 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> nums = new ArrayList<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums.add(Integer.parseInt(br.readLine()));
+        }
+        nums.sort(Comparator.naturalOrder());
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(nums.get(i)).append("\n");
+        }
+        System.out.println(result);
+    }
+}
