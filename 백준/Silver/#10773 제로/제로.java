467d7ff6d4a3bfd20b8f060e357baf28bf9fc544
Yong Hyeon <126778700+yonghyeonpark@users.noreply.github.com>
2024-11-22 23:23:20 +0900

    [Silver IV 10773] Title: 제로, Time: 216 ms, Memory: 23392 KB -NKLCBHub

---
+++ 백준/Silver/#10773 제로/제로.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> s = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                s.push(num);
+                continue;
+            }
+            s.pop();
+        }
+        
+        int sum = 0;
+        while (!s.isEmpty()) {
+            sum += s.pop();
+        }
+        System.out.println(sum);
+    }
+}
